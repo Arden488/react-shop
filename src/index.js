bc5e6ea860014ee5a461c897fc64c92a2039811c
Anton Samoilov <samoilov@aeroidea.ru>
2018-05-19 14:10:25 +0300

    Add react router and redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers';
+
+import './index.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import CatalogRoot from './components/CatalogRoot';
+import CatalogCategory from './components/CatalogCategory';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/catalog/:cat" component={CatalogCategory} />
+          <Route path="/catalog" component={CatalogRoot} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
