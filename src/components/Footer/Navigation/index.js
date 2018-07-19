ffedf752834a81221812ea86fc99160e41e336fe
Anton Samoilov <absamoilov88@gmail.com>
2018-07-19 16:02:41 +0300

    Add footer nav active link

--- src/components/Footer/Navigation/index.js
+++ src/components/Footer/Navigation/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import mainNavItems from '../../../config/mainNav';
 
 import './styles.css';
 
-const Navigation = () => (
-  <nav className="footer-navigation">
-    {mainNavItems.map(item => <Link className="footer-navigation__item" key={item.path} to={item.path}>{item.title}</Link>)}
-  </nav>
-);
+class Navigation extends Component {
+  renderLink(item) {
+    let linkClass = 'footer-navigation__item';
+    if (this.props.location.pathname === item.path) {
+      linkClass += ' footer-navigation__item_active';
+    }
+    return (
+      <Link
+        className={linkClass}
+        key={item.path}
+        to={item.path}
+      >
+        {item.title}
+      </Link>
+    );
+  }
 
-export default Navigation;
+  render() {
+    return (
+      <nav className="footer-navigation">
+        {mainNavItems.map(item => this.renderLink(item))}
+      </nav>
+    );
+  }
+}
+
+Navigation.propTypes = {
+  location: PropTypes.shape().isRequired,
+};
+
+export default withRouter(Navigation);
