356a12ea9a11e93edf603f2a6fbfedff10fe02e2
Anton Samoilov <absamoilov88@gmail.com>
2018-07-21 13:03:13 +0300

    Add global vars for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
-import { configure } from 'enzyme';
+import { shallow, render, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
