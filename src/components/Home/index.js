bad2bda5d025bcaacfff354bf4527e7169ff2438
Anton Samoilov <absamoilov88@gmail.com>
2018-07-30 23:12:31 +0300

    Add sliders and products list to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import Slider from '../Slider';
+import HotProducts from '../HotProducts';
+import FeaturedProducts from '../FeaturedProducts';
+import DiscountedProducts from '../DiscountedProducts';
+
 import './styles.css';
 
 const Home = () => (
   <div>
-    Home
+    <Slider />
+    
+    <h2>Top sale:</h2>
+    <HotProducts />
+
+    <h2>Featured</h2>
+    <FeaturedProducts />
+
+    <h2>Best discounts:</h2>
+    <DiscountedProducts />
   </div>
 );
 
