3188efe1ed43f16019675ba9e4798a15bfc0f438
Anton Samoilov <absamoilov88@gmail.com>
2018-08-04 23:45:10 +0300

    Add styles and filter to catalog category

--- src/components/CatalogCategory/index.tsx
+++ src/components/CatalogCategory/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 
-import CatalogElementList from '../CatalogElementList/index';
+import CatalogElementList from '../CatalogElementList';
+import CatalogFilter from '../CatalogFilter';
+
+import './styles.css';
 
 interface ICatalogCategoryProps {
   match: any
@@ -9,7 +12,10 @@ interface ICatalogCategoryProps {
 const CatalogCategory: React.SFC<ICatalogCategoryProps> = props => (
   <div>
     <h1>{props.match.params.cat}</h1>
-    <CatalogElementList />
+    <div className="catalog-detail">
+      <CatalogFilter />
+      <CatalogElementList items={[1, 2, 3, 4, 5]} />
+    </div>
   </div>
 );
 
