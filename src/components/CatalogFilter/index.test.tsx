d01eca5d1e64ad52ebfced8d9f0b654c1fbbc4fe
Anton Samoilov <absamoilov88@gmail.com>
2018-08-04 23:44:44 +0300

    Add catalog filter component

---
+++ src/components/CatalogFilter/index.test.tsx
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import CatalogFilter from './index';
+
+let component: React.ReactNode = null;
+
+beforeEach(() => {
+  component = shallow(<CatalogFilter />);
+});
+
+it('renders correctly', () => {
+  expect(component).toMatchSnapshot();
+});
---
+++ src/components/CatalogFilter/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const CatalogFilter = () => <div>CatalogFilter</div>;
+
+export default CatalogFilter;
