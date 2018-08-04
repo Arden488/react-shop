04b09fea1520dc8cda7990770e6892f4091674ca
Anton Samoilov <absamoilov88@gmail.com>
2018-08-04 23:43:53 +0300

    Update tests setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,4 @@
-import { shallow, render, mount, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
