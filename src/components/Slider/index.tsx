75b0fdf7e794ac76d75ef85d616d15a171a93134
Anton Samoilov <absamoilov88@gmail.com>
2018-08-07 23:43:16 +0300

    Add main slider basic layout

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -3,8 +3,10 @@ import * as React from 'react';
 import './styles.css';
 
 const Slider = () => (
-  <div>
-    Slider
+  <div className="main-slider">
+    <img className="main-slider__item main-slider__item_active" src="img/slide3.jpg" alt="" />
+    <img className="main-slider__item" src="img/slide2.jpg" alt="" />
+    <img className="main-slider__item" src="img/slide1.jpg" alt="" />
   </div>
 );
 
