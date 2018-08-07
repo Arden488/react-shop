862f633b4e51db92fec17bf512b684408887bf2d
Anton Samoilov <absamoilov88@gmail.com>
2018-08-07 23:43:03 +0300

    Remove slider from home page component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,14 +3,11 @@ import * as React from 'react';
 import DiscountedProducts from '../DiscountedProducts';
 import FeaturedProducts from '../FeaturedProducts';
 import HotProducts from '../HotProducts';
-import Slider from '../Slider';
 
 import './styles.css';
 
 const Home = () => (
   <div>
-    <Slider />
-    
     <h2>Top sale:</h2>
     <HotProducts />
 
