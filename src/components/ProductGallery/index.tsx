a2cead2b28e0fb59be1cf73e2b8e8b23e553e61b
Anton Samoilov <absamoilov88@gmail.com>
2018-08-08 23:47:05 +0300

    Add markup for product gallery

--- src/components/ProductGallery/index.tsx
+++ src/components/ProductGallery/index.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const ProductGallery = () => (
-  <div>ProductGallery</div>
+  <div className="product-gallery">
+    <img src="/img/iphone1.webp" alt="" className="product-gallery__item product-gallery__item_active" />
+    <img src="/img/iphone2.webp" alt="" className="product-gallery__item" />
+    <img src="/img/iphone3.webp" alt="" className="product-gallery__item" />
+    <img src="/img/iphone4.webp" alt="" className="product-gallery__item" />
+  </div>
 );
 
 export default ProductGallery;
