a9eefa5737468ce50760027a6e536b4396557422
Anton Samoilov <absamoilov88@gmail.com>
2018-08-09 23:10:10 +0300

    Add markup for color picker

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const ColorPicker = () => (
-  <div>ColorPicker</div>
+  <div className="color-picker">
+    <button type="button" className="color-picker__item color-picker__item_selected">
+    <span className="color-picker__color color-picker__color_gray" />
+      <span className="color-picker__label">Gray space</span>
+      <input className="color-picker__input" type="checkbox" name="" />
+    </button>
+    <button type="button" className="color-picker__item">
+      <span className="color-picker__color" />
+      <span className="color-picker__label">Black onyx</span>
+      <input className="color-picker__input" type="checkbox" name="" />
+    </button>
+  </div>
 );
 
 export default ColorPicker;
