e1bdf322bed5c458198774645805ae3b1aa2e3c9
Anton Samoilov <absamoilov88@gmail.com>
2018-08-10 23:19:55 +0300

    Add markup for controls

--- src/components/ProductDetailControls/index.tsx
+++ src/components/ProductDetailControls/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const ProductDetailControls = () => (
-  <div>ProductDetailControls</div>
+  <div className="product-controls">
+    <button className="b-btn">Add to cart</button>
+  </div>
 );
 
 export default ProductDetailControls;
