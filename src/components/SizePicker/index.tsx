68a13f85c45144a3ef6e07a04dc18ff2d14ee669
Anton Samoilov <absamoilov88@gmail.com>
2018-08-10 23:20:36 +0300

    Add styles and markup for size picker

--- src/components/SizePicker/index.tsx
+++ src/components/SizePicker/index.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const SizePicker = () => (
-  <div>SizePicker</div>
+  <div className="size-picker">
+    <button type="button" className="size-picker__item size-picker__item_selected">
+      <span className="size-picker__label">16 Gb</span>
+      <input className="size-picker__input" type="checkbox" name="" />
+    </button>
+    <button type="button" className="size-picker__item">
+      <span className="size-picker__label">32 Gb</span>
+      <input className="size-picker__input" type="checkbox" name="" />
+    </button>
+    <button type="button" className="size-picker__item">
+      <span className="size-picker__label">128 Gb</span>
+      <input className="size-picker__input" type="checkbox" name="" />
+    </button>
+    <button type="button" className="size-picker__item">
+      <span className="size-picker__label">256 Gb</span>
+      <input className="size-picker__input" type="checkbox" name="" />
+    </button>
+    <button type="button" className="size-picker__item">
+      <span className="size-picker__label">512 Gb</span>
+      <input className="size-picker__input" type="checkbox" name="" />
+    </button>
+  </div>
 );
 
 export default SizePicker;
