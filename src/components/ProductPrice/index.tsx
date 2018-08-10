08283da9b40cbedbd500ee18b89322ac3f4436f1
Anton Samoilov <absamoilov88@gmail.com>
2018-08-10 23:20:19 +0300

    Add markup and styles for price

--- src/components/ProductPrice/index.tsx
+++ src/components/ProductPrice/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const ProductPrice = () => (
-  <div>ProductPrice</div>
+  <div className="product-price">
+    <span className="product-price__main-value">
+      $520
+      <span className="product-price__discount">-20%</span>
+    </span>
+    <span className="product-price__old-value">$650</span>
+  </div>
 );
 
 export default ProductPrice;
