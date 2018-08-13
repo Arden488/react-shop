a69e3069cd4a725590f397efed0e2ba06ef3e470
Anton Samoilov <absamoilov88@gmail.com>
2018-08-13 22:57:47 +0300

    Refactor tab component

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 
+import './styles.css';
+
 interface ITabProps {
   children: any,
   label: string,
+  active?: boolean,
 }
 
-const Tab: React.SFC<ITabProps> = (props: any) => (
-  <div>{props.children}</div>
-);
+class Tab extends React.Component<ITabProps> {
+  public render() {
+    let classes:string = 'tab';
+    if (this.props.active) {
+      classes += 'tab_active';
+    }
+
+    return (
+      <div className={classes}>{this.props.children}</div>
+    );
+  }
+}
 
 export default Tab;
