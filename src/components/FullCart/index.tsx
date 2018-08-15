f35bb77ab3e9362a2ff5caa03242e439eedba1ae
Anton Samoilov <absamoilov88@gmail.com>
2018-08-15 23:18:16 +0300

    Add demo markup and styles for full cart

--- src/components/FullCart/index.tsx
+++ src/components/FullCart/index.tsx
@@ -1,7 +1,53 @@
 import * as React from 'react';
 
+import './styles.css';
+
 const FullCart = () => (
-  <div>FullCart</div>
+  <div>
+    <h1>Cart</h1>
+    <div className="full-cart">
+      <div className="full-cart-item">
+        <img className="full-cart-item__image" src="/img/iphone8_red.webp" alt="Apple iPhone 8 64 ГБ (PRODUCT)RED" />
+        <div className="full-cart-item__title">Apple iPhone 8 64 ГБ (PRODUCT)RED</div>
+        <div className="full-cart-item__controls">
+          <div className="full-cart-item__qnt">Quantity</div>
+          <div className="full-cart-item__price">39 990 ₽</div>
+          <div className="full-cart-item__remove">
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
+      <div className="full-cart-item">
+      <img className="full-cart-item__image" src="/img/iphone8_red.webp" alt="Apple iPhone 8 64 ГБ (PRODUCT)RED" />
+        <div className="full-cart-item__title">Apple iPhone 8 64 ГБ (PRODUCT)RED</div>
+        <div className="full-cart-item__controls">
+          <div className="full-cart-item__qnt">Quantity</div>
+          <div className="full-cart-item__price">39 990 ₽</div>
+          <div className="full-cart-item__remove">
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
+      <div className="full-cart-item">
+      <img className="full-cart-item__image" src="/img/iphone8_red.webp" alt="Apple iPhone 8 64 ГБ (PRODUCT)RED" />
+        <div className="full-cart-item__title">Apple iPhone 8 64 ГБ (PRODUCT)RED</div>
+        <div className="full-cart-item__controls">
+          <div className="full-cart-item__qnt">Quantity</div>
+          <div className="full-cart-item__price">39 990 ₽</div>
+          <div className="full-cart-item__remove">
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="full-cart__summ">
+        Summary:
+        <span className="full-cart__summ-value">39 990 ₽</span>
+      </div>
+
+      <button className="full-cart__proceed">Proceed to checkout</button>
+    </div>
+  </div>
 );
 
 export default FullCart;
