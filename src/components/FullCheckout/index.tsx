8cafa86866349dbe1e048f56989e0afb06b636d3
Anton Samoilov <absamoilov88@gmail.com>
2018-08-20 22:30:50 +0300

    Add checkout component dummy

---
+++ src/components/FullCheckout/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import './styles.css';
+
+const FullCheckout = () => (
+  <div>
+    <h1>Checkout</h1>
+    <p>Checkout</p>
+  </div>
+);
+
+export default FullCheckout;
