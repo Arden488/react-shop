c65fefc42fb624dcb7c8ba1accbc59ce01573d37
Anton Samoilov <absamoilov88@gmail.com>
2018-08-21 22:45:27 +0300

    Add placeholders for parts of checkout

--- src/components/FullCheckout/index.tsx
+++ src/components/FullCheckout/index.tsx
@@ -4,8 +4,26 @@ import './styles.css';
 
 const FullCheckout = () => (
   <div>
-    <h1>Checkout</h1>
-    <p>Checkout</p>
+    <div>
+      <h2>Delivery</h2>
+      <h3>Type</h3>
+      <p>Types of delivery</p>
+      <h3>Address</h3>
+      <p>Adress inputs</p>
+      
+      <hr />
+      
+      <h2>Buyer details</h2>
+      <p>Information about buyer</p>
+      
+      <hr />
+      
+      <h2>Payment</h2>
+      <p>Types of payment</p>
+    </div>
+    <div>
+      Summary
+    </div>
   </div>
 );
 
