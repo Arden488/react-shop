cf78cb462fbd51b47873e05572b4cc40230b9a68
Anton Samoilov <absamoilov88@gmail.com>
2018-08-24 23:55:42 +0300

    Pass dummy data to list

--- src/components/CatalogCategory/index.tsx
+++ src/components/CatalogCategory/index.tsx
@@ -5,24 +5,52 @@ import CatalogFilter from '../CatalogFilter';
 import CatalogSort from '../CatalogSort';
 
 import './styles.css';
+import { render } from 'react-dom';
 
 interface ICatalogCategoryProps {
   match: any
 }
 
-const CatalogCategory: React.SFC<ICatalogCategoryProps> = props => (
-  <div>
-    <h1>{props.match.params.cat}</h1>
-    <div className="catalog-category-wrapper">
-      <div className="catalog-category__aside">
-        <CatalogFilter />
+class CatalogCategory extends React.Component<ICatalogCategoryProps> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      items: [{
+        id: 1,
+        image: "iphone8_red.webp",
+        price: "39990",
+        title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+      }, {
+        id: 2,
+        image: "iphone8_red.webp",
+        price: "49990",
+        title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+      }, {
+        id: 3,
+        image: "iphone8_red.webp",
+        price: "59990",
+        title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+      }]
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        <h1>{this.props.match.params.cat}</h1>
+        <div className="catalog-category-wrapper">
+          <div className="catalog-category__aside">
+            <CatalogFilter />
+          </div>
+          <div className="catalog-category__content">
+            <CatalogSort />
+            <CatalogElementList items={this.state.items} />
+          </div>
+        </div>
       </div>
-      <div className="catalog-category__content">
-        <CatalogSort />
-        <CatalogElementList items={[1, 2, 3, 4, 5]} />
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 export default CatalogCategory;
