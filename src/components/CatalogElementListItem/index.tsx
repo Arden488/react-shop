a2bdc2c1148f1eb62ae255dd8f943d5255136b2f
Anton Samoilov <absamoilov88@gmail.com>
2018-08-25 23:13:30 +0300

    Refactor catalog element list item to show passed props

--- src/components/CatalogElementListItem/index.tsx
+++ src/components/CatalogElementListItem/index.tsx
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 interface ICatalogElementListItemProps {
-  code: string,
+  id: number,
   key: number,
+  title: string,
+  price: string,
+  image: string,
 }
 
 const CatalogElementListItem: React.SFC<ICatalogElementListItemProps> = props => (
   <div className="product-item">
-    <Link className="product-item__link" to={`/product/${props.code}`}>
-      <div className="product-item__image"><img src="/img/iphone8_red.webp" alt="Apple iPhone 8 64 ГБ (PRODUCT)RED" /></div>
-      <div className="product-item__title">Apple iPhone 8 64 ГБ (PRODUCT)RED</div>
+    <Link className="product-item__link" to={`/product/${props.id}`}>
+      <div className="product-item__image"><img src={`/img/${props.image}`} alt={props.title} /></div>
+      <div className="product-item__title">{props.title}</div>
       <div className="product-item__controls">
-        <div className="product-item__price">39 990 ₽</div>
+        <div className="product-item__price">{props.price} ₽</div>
         <div className="product-item__buy">
           <button>Купить</button>
         </div>
