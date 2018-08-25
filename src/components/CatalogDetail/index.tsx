33f983e6505c71ed516a9f563b5320f55af5c7c1
Anton Samoilov <absamoilov88@gmail.com>
2018-08-25 23:10:08 +0300

    Pass demo data to catalog detail component

--- src/components/CatalogDetail/index.tsx
+++ src/components/CatalogDetail/index.tsx
@@ -16,78 +16,109 @@ interface ICatalogDetailProps {
   match: any,
 }
 
-const CatalogDetail: React.SFC<ICatalogDetailProps> = props => (
-  <div>
-    <div className="catalog-detail">
-      <div className="catalog-detail__content">
-        <div className="catalog-detail__gallery">
-          <ProductGallery />
-        </div>
-        <div className="catalog-detail__info">
-          <h1>{props.match.params.code}</h1>
-          <p>Some description</p>
+interface ICatalogDetailState {
+  items: Array<{ id: number, image: string, price: string, title: string }>
+}
 
-          <div>
-            <p>Choose options:</p>
-            <div className="catalog-detail__opt-section">
-              <ColorPicker />
+class CatalogDetail extends React.Component<ICatalogDetailProps, ICatalogDetailState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      items: [{
+        id: 1,
+        image: "iphone8_red.webp",
+        price: "39990",
+        title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+      }, {
+        id: 2,
+        image: "iphone8_red.webp",
+        price: "49990",
+        title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+      }, {
+        id: 3,
+        image: "iphone8_red.webp",
+        price: "59990",
+        title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+      }]
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        <div className="catalog-detail">
+          <div className="catalog-detail__content">
+            <div className="catalog-detail__gallery">
+              <ProductGallery />
             </div>
-            <div className="catalog-detail__opt-section">
-              <SizePicker />
+            <div className="catalog-detail__info">
+              <h1>{this.props.match.params.code}</h1>
+              <p>Some description</p>
+
+              <div>
+                <p>Choose options:</p>
+                <div className="catalog-detail__opt-section">
+                  <ColorPicker />
+                </div>
+                <div className="catalog-detail__opt-section">
+                  <SizePicker />
+                </div>
+              </div>
+              <div className="catalog-detail__opt-section">
+                <ProductPrice />
+              </div>
+              <div className="catalog-detail__opt-section">
+                <ProductDetailControls />
+              </div>
+              <div className="catalog-detail__opt-section">
+                <DeliveryOptions />
+              </div>
             </div>
           </div>
-          <div className="catalog-detail__opt-section">
-            <ProductPrice />
+          <div className="catalog-detail__additional">
+            <Tabs>
+              <Tab label="Description">Description</Tab>
+              <Tab label="Characteristics" active={true}>
+                <table className="tech-table">
+                  <tbody>
+                    <tr>
+                      <th colSpan={2}>Main specs</th>
+                    </tr>
+                    <tr>
+                      <td>Capacity</td>
+                      <td>256 Gb</td>
+                    </tr>
+                    <tr>
+                      <td>Color</td>
+                      <td>Space gray</td>
+                    </tr>
+                    <tr>
+                      <td>Size and weight</td>
+                      <td>
+                        Height: 5.65 inches (143.6 mm)<br />
+                        Width: 2.79 inches (70.9 mm)<br />
+                        Depth: 0.30 inch (7.7 mm)<br />
+                        Weight: 6.14 ounces (174 grams)
+                      </td>
+                    </tr>
+                    <tr>
+                      <th colSpan={2}>Other</th>
+                    </tr>
+                    <tr>
+                      <td>Splash, Water, and Dust Resistant</td>
+                      <td>Rated IP67 under IEC standard 60529</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Tab>
+            </Tabs>
           </div>
-          <div className="catalog-detail__opt-section">
-            <ProductDetailControls />
-          </div>
-          <div className="catalog-detail__opt-section">
-            <DeliveryOptions />
-          </div>
+          <FeaturedProducts items={this.state.items} />
         </div>
       </div>
-      <div className="catalog-detail__additional">
-        <Tabs>
-          <Tab label="Description">Description</Tab>
-          <Tab label="Characteristics" active={true}>
-            <table className="tech-table">
-              <tbody>
-                <tr>
-                  <th colSpan={2}>Main specs</th>
-                </tr>
-                <tr>
-                  <td>Capacity</td>
-                  <td>256 Gb</td>
-                </tr>
-                <tr>
-                  <td>Color</td>
-                  <td>Space gray</td>
-                </tr>
-                <tr>
-                  <td>Size and weight</td>
-                  <td>
-                    Height: 5.65 inches (143.6 mm)<br />
-                    Width: 2.79 inches (70.9 mm)<br />
-                    Depth: 0.30 inch (7.7 mm)<br />
-                    Weight: 6.14 ounces (174 grams)
-                  </td>
-                </tr>
-                <tr>
-                  <th colSpan={2}>Other</th>
-                </tr>
-                <tr>
-                  <td>Splash, Water, and Dust Resistant</td>
-                  <td>Rated IP67 under IEC standard 60529</td>
-                </tr>
-              </tbody>
-            </table>
-          </Tab>
-        </Tabs>
-      </div>
-      <FeaturedProducts items={[1, 2, 3]} />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default CatalogDetail;
