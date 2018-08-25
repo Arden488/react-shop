2e5a5de7e1cd2093d7c9de08d61c7fed227e8c32
Anton Samoilov <absamoilov88@gmail.com>
2018-08-25 23:11:22 +0300

    Pass demo data to components

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,17 +6,84 @@ import HotProducts from '../HotProducts';
 
 import './styles.css';
 
-const Home = () => (
-  <div>
-    <h2>Top sale:</h2>
-    <HotProducts items={[1, 2, 3]} />
+// interface IHomeProps {}
 
-    <h2>Featured</h2>
-    <FeaturedProducts items={[1, 2, 3]} />
+interface IHomeState {
+  dpItems: Array<{ id: number, image: string, price: string, title: string }>,
+  fpItems: Array<{ id: number, image: string, price: string, title: string }>,
+  hpItems: Array<{ id: number, image: string, price: string, title: string }>,
+}
 
-    <h2>Best discounts:</h2>
-    <DiscountedProducts items={[1, 2, 3]} />
-  </div>
-);
+class Home extends React.Component<any, IHomeState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      dpItems: [{
+        id: 1,
+        image: "iphone8_red.webp",
+        price: "39990",
+        title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+      }, {
+        id: 2,
+        image: "iphone8_red.webp",
+        price: "49990",
+        title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+      }, {
+        id: 3,
+        image: "iphone8_red.webp",
+        price: "59990",
+        title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+      }],
+      fpItems: [{
+        id: 1,
+        image: "iphone8_red.webp",
+        price: "39990",
+        title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+      }, {
+        id: 2,
+        image: "iphone8_red.webp",
+        price: "49990",
+        title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+      }, {
+        id: 3,
+        image: "iphone8_red.webp",
+        price: "59990",
+        title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+      }],
+      hpItems: [{
+        id: 1,
+        image: "iphone8_red.webp",
+        price: "39990",
+        title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+      }, {
+        id: 2,
+        image: "iphone8_red.webp",
+        price: "49990",
+        title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+      }, {
+        id: 3,
+        image: "iphone8_red.webp",
+        price: "59990",
+        title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+      }],
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        <h2>Top sale:</h2>
+        <HotProducts items={this.state.hpItems} />
+
+        <h2>Featured</h2>
+        <FeaturedProducts items={this.state.fpItems} />
+
+        <h2>Best discounts:</h2>
+        <DiscountedProducts items={this.state.dpItems} />
+      </div>
+    );
+  }
+}
 
 export default Home;
