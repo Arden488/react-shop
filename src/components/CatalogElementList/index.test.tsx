55963533463604d303984c93f3a8293d7a6b7ff5
Anton Samoilov <absamoilov88@gmail.com>
2018-09-04 23:16:29 +0300

    Update tests

--- src/components/CatalogElementList/index.test.tsx
+++ src/components/CatalogElementList/index.test.tsx
@@ -7,20 +7,26 @@ let component: React.ReactNode = null;
 
 beforeEach(() => {
   const items = [{
+    article: 762015,
     id: 1,
     image: "iphone8_red.webp",
     price: "39990",
     title: "Apple iPhone 8 32 ГБ (PRODUCT)RED",
+    type: "iphone7",
   }, {
+    article: 762016,
     id: 2,
     image: "iphone8_red.webp",
     price: "49990",
     title: "Apple iPhone 8 64 ГБ (PRODUCT)RED",
+    type: "iphone8",
   }, {
+    article: 762017,
     id: 3,
     image: "iphone8_red.webp",
     price: "59990",
     title: "Apple iPhone 8 256 ГБ (PRODUCT)RED",
+    type: "iphone7",
   }]
   component = shallow(<CatalogElementList items={items} />);
 });
