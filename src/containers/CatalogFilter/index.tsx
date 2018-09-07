eae77d1ecafa21fd06e213438c646cc21f0762fd
Anton Samoilov <absamoilov88@gmail.com>
2018-09-07 19:46:37 +0300

    Add additional fields to filter and process

--- src/containers/CatalogFilter/index.tsx
+++ src/containers/CatalogFilter/index.tsx
@@ -52,10 +52,30 @@ class CatalogFilter extends React.Component<ICatalogFilterProps> {
         <div className="catalog-filter__section">
           <div>Color:</div>
           <div>
-            <input type="checkbox" value="" name="" /> Gray space<br />
-            <input type="checkbox" value="" name="" /> Black<br />
-            <input type="checkbox" value="" name="" /> Gold<br />
-            <input type="checkbox" value="" name="" /> Gold rose
+            <input 
+              type="checkbox"
+              className="products-filter-property"
+              value="gray_space" 
+              onChange={this.handlePropertyCheck}
+              name="filter_color" /> Gray space<br />
+            <input 
+              type="checkbox"
+              className="products-filter-property"
+              value="black" 
+              onChange={this.handlePropertyCheck}
+              name="filter_color" /> Black<br />
+            <input 
+              type="checkbox"
+              className="products-filter-property"
+              value="gold" 
+              onChange={this.handlePropertyCheck}
+              name="filter_color" /> Gold<br />
+            <input 
+              type="checkbox"
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="gold_rose" 
+              name="filter_color" /> Gold rose
           </div>
         </div>
         <div className="catalog-filter__section">
@@ -65,30 +85,95 @@ class CatalogFilter extends React.Component<ICatalogFilterProps> {
         <div className="catalog-filter__section">
           <div>Screen size:</div>
           <div>
-            <input type="checkbox" value="" name="" /> 4'<br />
-            <input type="checkbox" value="" name="" /> 5'<br />
-            <input type="checkbox" value="" name="" /> 6'
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="4" 
+              name="filter_screen_size" /> 4'<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="5" 
+              name="filter_screen_size" /> 5'<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="6" 
+              name="filter_screen_size" /> 6'
           </div>
         </div>
         <div className="catalog-filter__section">
           <div>Memory size:</div>
           <div>
-            <input type="checkbox" value="" name="" /> 16 Gb<br />
-            <input type="checkbox" value="" name="" /> 32 Gb<br />
-            <input type="checkbox" value="" name="" /> 64 Gb<br />
-            <input type="checkbox" value="" name="" /> 128 Gb<br />
-            <input type="checkbox" value="" name="" /> 256 Gb
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="16" 
+              name="filter_memory_size" /> 16 Gb<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="32" 
+              name="filter_memory_size" /> 32 Gb<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="64" 
+              name="filter_memory_size" /> 64 Gb<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="128" 
+              name="filter_memory_size" /> 128 Gb<br />
+            <input 
+              type="checkbox" 
+              className="products-filter-property"
+              onChange={this.handlePropertyCheck}
+              value="256" 
+              name="filter_memory_size" /> 256 Gb
           </div>
         </div>
         <div className="catalog-filter__section">
           <div>Release year:</div>
           <div>
             <div>
-              <input type="checkbox" value="" name="" /> 2012<br />
-              <input type="checkbox" value="" name="" /> 2011<br />
-              <input type="checkbox" value="" name="" /> 2010<br />
-              <input type="checkbox" value="" name="" /> 2009<br />
-              <input type="checkbox" value="" name="" /> 2008
+              <input 
+                type="checkbox" 
+                className="products-filter-property"
+                onChange={this.handlePropertyCheck}
+                value="2012" 
+                name="filter_year" /> 2012<br />
+              <input 
+                type="checkbox" 
+                className="products-filter-property"
+                onChange={this.handlePropertyCheck}
+                value="2011" 
+                name="filter_year" /> 2011<br />
+              <input 
+                type="checkbox" 
+                className="products-filter-property"
+                onChange={this.handlePropertyCheck}
+                value="2010" 
+                name="filter_year" /> 2010<br />
+              <input 
+                type="checkbox" 
+                className="products-filter-property"
+                onChange={this.handlePropertyCheck}
+                value="2009" 
+                name="filter_year" /> 2009<br />
+              <input 
+                type="checkbox" 
+                className="products-filter-property"
+                onChange={this.handlePropertyCheck}
+                value="2008" 
+                name="filter_year" /> 2008
             </div>
             <a href="#">more</a>
           </div>
@@ -98,15 +183,17 @@ class CatalogFilter extends React.Component<ICatalogFilterProps> {
   }
 
   private handlePropertyCheck() {
-    const type: any = [];
     const checks = document.querySelectorAll('.products-filter-property');
+    const options = {};
 
     checks.forEach((el: any) => {
       if (el.checked) {
-        type.push(el.value);
+        if ( !options[el.name] ) {
+          options[el.name] = [];
+        }
+        options[el.name].push(el.value);
       }
     });
-    const options = { type };
     this.props.filterProducts(options);
   }
 }
