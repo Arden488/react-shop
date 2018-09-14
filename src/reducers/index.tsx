c4dda0f65e45f7fc24f43f9aa9ca66a2d751d572
Anton Samoilov <absamoilov88@gmail.com>
2018-09-14 23:59:14 +0300

    Update combined reducers

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,11 +1,13 @@
 // import { combineReducers } from 'redux';
 
 import ProductsFilter from './reducer_filter_products';
+import Filters from './reducer_filters';
 import ProductsReducer from './reducer_products';
 
 export default (state = {}, action: any) => {
   return {
     ...ProductsReducer({}, action, state),
     ...ProductsFilter({}, action, state),
+    ...Filters({}, action, state),
   }
 };
