7b2eba1624baaf850dbb58ca31147131b60cb884
Anton Samoilov <absamoilov88@gmail.com>
2018-09-16 22:30:05 +0300

    Refactor catalog filter

--- src/containers/CatalogFilter/index.tsx
+++ src/containers/CatalogFilter/index.tsx
@@ -7,6 +7,7 @@ import './styles.css';
 
 interface ICatalogFilterProps {
   products: any,
+  filters: any,
   filterProducts: any,
   fetchFiltersByCategory: any,
 }
@@ -41,189 +42,106 @@ class CatalogFilter extends React.Component<ICatalogFilterProps, ICatalogFilterS
   }
 
   public render() {
+    const filters = this.props.filters;
+    const filterSections = this.outputFilters(filters);
+    
     return (
       <div>
-        <div className="catalog-filter__section">
-          <div>Product series:</div>
-          <div>
-            <input 
-              type="checkbox" 
-              value="iphonex" 
-              onChange={this.handlePropertyCheck} 
-              className="products-filter-property" 
-              name="filter_type" /> iPhone X<br />
-            <input 
-              type="checkbox" 
-              value="iphone8" 
-              onChange={this.handlePropertyCheck} 
-              className="products-filter-property" 
-              name="filter_type" /> iPhone 8<br />
-            <input 
-              type="checkbox" 
-              value="iphone7" 
-              onChange={this.handlePropertyCheck} 
-              className="products-filter-property" 
-              name="filter_type" /> iPhone 7<br />
-            <input 
-              type="checkbox" 
-              value="iphone6" 
-              onChange={this.handlePropertyCheck} 
-              className="products-filter-property" 
-              name="filter_type" /> iPhone 6<br />
-          </div>
-        </div>
-        <div className="catalog-filter__section">
-          <div>Color:</div>
-          <div>
-            <input 
-              type="checkbox"
-              className="products-filter-property"
-              value="gray_space" 
-              onChange={this.handlePropertyCheck}
-              name="filter_color" /> Gray space<br />
-            <input 
-              type="checkbox"
-              className="products-filter-property"
-              value="black" 
-              onChange={this.handlePropertyCheck}
-              name="filter_color" /> Black<br />
-            <input 
-              type="checkbox"
-              className="products-filter-property"
-              value="gold" 
-              onChange={this.handlePropertyCheck}
-              name="filter_color" /> Gold<br />
-            <input 
-              type="checkbox"
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="gold_rose" 
-              name="filter_color" /> Gold rose
-          </div>
-        </div>
-        <div className="catalog-filter__section">
-          <div>Price range:</div>
-          <div>
-            From $
-            <input 
-              type="text" 
-              className="products-filter-property products-filter-price-from"
-              name="filter_price_from" 
-              value={this.state.priceFrom} 
-              onChange={this.handlePropertyPriceRange}
-              size={4} /> 
-            to $
-            <input 
-              type="text" 
-              className="products-filter-property products-filter-price-to"
-              name="filter_price_to" 
-              value={this.state.priceTo}
-              onChange={this.handlePropertyPriceRange}
-              size={4} />
-          </div>
-        </div>
-        <div className="catalog-filter__section">
-          <div>Screen size:</div>
-          <div>
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="4" 
-              name="filter_screen_size" /> 4'<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="5" 
-              name="filter_screen_size" /> 5'<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="6" 
-              name="filter_screen_size" /> 6'
-          </div>
-        </div>
-        <div className="catalog-filter__section">
-          <div>Memory size:</div>
-          <div>
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="16" 
-              name="filter_memory_size" /> 16 Gb<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="32" 
-              name="filter_memory_size" /> 32 Gb<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="64" 
-              name="filter_memory_size" /> 64 Gb<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="128" 
-              name="filter_memory_size" /> 128 Gb<br />
-            <input 
-              type="checkbox" 
-              className="products-filter-property"
-              onChange={this.handlePropertyCheck}
-              value="256" 
-              name="filter_memory_size" /> 256 Gb
-          </div>
-        </div>
-        <div className="catalog-filter__section">
-          <div>Release year:</div>
-          <div>
-            <div>
-              <input 
-                type="checkbox" 
-                className="products-filter-property"
-                onChange={this.handlePropertyCheck}
-                value="2012" 
-                name="filter_year" /> 2012<br />
-              <input 
-                type="checkbox" 
-                className="products-filter-property"
-                onChange={this.handlePropertyCheck}
-                value="2011" 
-                name="filter_year" /> 2011<br />
-              <input 
-                type="checkbox" 
-                className="products-filter-property"
-                onChange={this.handlePropertyCheck}
-                value="2010" 
-                name="filter_year" /> 2010<br />
-              <input 
-                type="checkbox" 
-                className="products-filter-property"
-                onChange={this.handlePropertyCheck}
-                value="2009" 
-                name="filter_year" /> 2009<br />
-              <input 
-                type="checkbox" 
-                className="products-filter-property"
-                onChange={this.handlePropertyCheck}
-                value="2008" 
-                name="filter_year" /> 2008
-            </div>
-            <a href="#">more</a>
-          </div>
-        </div>
+        {filterSections}
       </div>
     )
   }
 
   public componentDidMount() {
     this.props.fetchFiltersByCategory('ipad');
+  }
+
+  private getFilterFields(
+      name: string,
+      fieldType: string, 
+      variants: [] = [], 
+      options: { min: string, max: string } = { max: '', min: '' }
+    ) {
+    switch (fieldType) {
+      case 'checkbox':
+        return this.outputCheckboxes(name, variants);
+      case 'range':
+        return this.outputRange(name, options);
+      default:
+        return '';
+    }
+  }
+
+  private getFilterSection(filter: { 
+      name: string, 
+      field_type: string, 
+      variants: [], 
+      options: { min: string, max: string } 
+    }) {
+    const fields = this.getFilterFields(filter.name, filter.field_type, filter.variants, filter.options);
+
+    return (
+      <div className="catalog-filter__section">
+        <div>{filter.name}:</div>
+        <div>
+          {fields}
+        </div>
+      </div>
+    )
+  }
+
+  private outputCheckboxes(name: string, variants: []) {
+    const checkboxes = variants.map((item: string) => {
+      return (
+        <div key={item}>
+          <input 
+            type="checkbox" 
+            value={item} 
+            onChange={this.handlePropertyCheck} 
+            className="products-filter-property" 
+            name={name} /> 
+          {item}
+        </div>
+      )
+    });
+
+    return checkboxes;
+  }
+
+  private outputFilters(filters: []) {
+    const filterSections = [];
+
+    for (const prop in filters) {
+      if (filters.hasOwnProperty(prop)) {
+        const params = filters[prop];
+        filterSections.push(this.getFilterSection(params));
+      }
+    }
+
+    return filterSections;
+  }
+
+  private outputRange(name: string, options: { min: string, max: string }) {
+    return (
+      <div>
+        From $
+        <input 
+          type="text" 
+          className="products-filter-property products-filter-price-from"
+          name={`{name}[]`}
+          value={options.min} 
+          onChange={this.handlePropertyPriceRange}
+          size={4} /> 
+        to $
+        <input 
+          type="text" 
+          className="products-filter-property products-filter-price-to"
+          name={`{name}[]`}
+          value={options.max}
+          onChange={this.handlePropertyPriceRange}
+          size={4} />
+      </div>
+    )
   }
 
   private handlePropertyCheck() {
