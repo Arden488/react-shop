f88c9dc196ddd9f212ce178ead1424ea77eae800
Anton Samoilov <absamoilov88@gmail.com>
2018-09-19 18:44:33 +0300

    Rename action type

--- src/actions/types.tsx
+++ src/actions/types.tsx
@@ -1,3 +1,3 @@
 export const fetchProductsSuccess = 'PRODUCTS_RECEIVED';
-export const filterProductsAction = 'PRODUCTS_FILTER';
 export const fetchProductsFilter = 'PRODUCTS_FILTER_RECEIVED';
+export const passFiltersSuccess = 'FILTER_PASSED';
