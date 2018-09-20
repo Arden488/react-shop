61683356f1455be9726c1d2f01e92a890bfcf481
Anton Samoilov <absamoilov88@gmail.com>
2018-09-20 22:14:44 +0300

    Update tests

--- src/containers/CatalogCategory/index.test.tsx
+++ src/containers/CatalogCategory/index.test.tsx
@@ -1,12 +1,23 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import CatalogCategory from './index';
 
+const mockStore = configureStore([thunk]);
+let store: any;
+const initialState = {
+  filters: { data: {} },
+  products: { data: [] },
+  productsFilterOptions: { data: {} },
+};
 let component: React.ReactNode = null;
 
 beforeEach(() => {
-  component = shallow(<CatalogCategory match={{ params: { cat: 'test' } }} />);
+  store = mockStore(initialState);
+  component = mount(<Provider store={store}><CatalogCategory match={{ params: { cat: 'test' } }} /></Provider>);
 });
 
 it('renders correctly', () => {
--- src/containers/CatalogFilter/index.test.tsx
+++ src/containers/CatalogFilter/index.test.tsx
@@ -1,12 +1,30 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import CatalogFilter from './index';
 
+const mockStore = configureStore([thunk]);
+let store: any;
+const initialState = {
+  filters: { data: {
+    type: {
+      field_type: "checkbox",
+      name: 'type',
+      title: 'Type',
+      variants: ["iPad 3", "iPad 4", "iPad 5", "iPad 6"]
+    }
+  } },
+  products: { data: [] },
+  productsFilterOptions: { data: {} },
+};
 let component: React.ReactNode = null;
 
 beforeEach(() => {
-  component = shallow(<CatalogFilter />);
+  store = mockStore(initialState);
+  component = mount(<Provider store={store}><CatalogFilter /></Provider>);
 });
 
 it('renders correctly', () => {
