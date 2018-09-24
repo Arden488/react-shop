f3728efbe7c59fc38f7cb6317bd413d9e0761183
Anton Samoilov <absamoilov88@gmail.com>
2018-09-24 21:28:59 +0300

    Add more filters to test its rendering

--- src/containers/CatalogFilter/index.test.tsx
+++ src/containers/CatalogFilter/index.test.tsx
@@ -2,6 +2,7 @@ import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 
 import { Provider } from 'react-redux';
+import { MemoryRouter, Router } from 'react-router';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import CatalogFilter from './index';
@@ -10,21 +11,34 @@ const mockStore = configureStore([thunk]);
 let store: any;
 const initialState = {
   filters: { data: {
+    price_range: {
+      field_type: "range",
+      name: "price",
+      options: {
+        max: "1000",
+        min: "300"
+      },
+      title: "Price range"
+    },
     type: {
       field_type: "checkbox",
       name: 'type',
       title: 'Type',
       variants: ["iPad 3", "iPad 4", "iPad 5", "iPad 6"]
-    }
+    },
   } },
   products: { data: [] },
   productsFilterOptions: { data: {} },
 };
-let component: React.ReactNode = null;
+let component: any = null;
 
 beforeEach(() => {
   store = mockStore(initialState);
-  component = mount(<Provider store={store}><CatalogFilter /></Provider>);
+  component = mount(<Provider store={store}>
+      <MemoryRouter initialEntries={[ { pathname: '/catalog/ipad', key: 'abcdef' } ]}>
+        <CatalogFilter />
+      </MemoryRouter>
+    </Provider>);
 });
 
 it('renders correctly', () => {
